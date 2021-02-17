16e968d817d88c33168b7938af9f7cad330e2751
Mukaila Semiu <semiteagain@gmail.com>
2021-02-17 21:38:38 +0100

    Jsx

--- src/index.js
+++ src/index.js
@@ -3,7 +3,11 @@ import ReactDOM from 'react-dom';
 
 
 ReactDOM.render(
-   <h1>Hello World</h1> ,
+   <div>
+   <h1>Hello World!</h1>
+   <h2>Day 2 learn Jsx </h2>
+   </div>
+    ,
   document.getElementById('root')
 );
 
